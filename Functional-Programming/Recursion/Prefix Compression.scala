b2d5bbea6706231bfbae344c37b6b223b7b48972
iMadooc <i.am.madooc@gmail.com>
2016-03-08 03:12:22 +0700

    Update and rename Prefix Compression to Prefix Compression.scala

--- Functional-Programming/Recursion/Prefix Compression.scala
+++ Functional-Programming/Recursion/Prefix Compression.scala
@@ -4,7 +4,6 @@ object Solution {
     val n = (x + 1 zip y + 2).indexWhere(x => x._1 != x._2)
     val a = x.splitAt(n)
     val b = y.splitAt(n)
-
     a._1.length +" "+ a._1 :: a._2.length +" "+ a._2 :: b._2.length +" "+ b._2 :: Nil
   }
 
