971abaca812f9d5171cb827cc1f6ac5e9ad73549
iMadooc <i.am.madooc@gmail.com>
2016-03-08 05:17:03 +0700

    Update Lists and GCD.scala

--- Functional-Programming/Functional-Structures/Lists and GCD.scala
+++ Functional-Programming/Functional-Structures/Lists and GCD.scala
@@ -4,15 +4,15 @@ object Solution {
     def mkIntIntList(s: String): List[(Int,Int)] =
       s.split(" ").map(_.toInt).sliding(2,2).toList.map(arr => (arr(0), arr(1)))
 
-    def gcf(lst: List[List[(Int,Int)]], gcfLst: List[(Int,Int)]): List[(Int,Int)] =
-      if (lst.isEmpty) gcfLst
-      else gcf(lst.tail, extract(lst.head, gcfLst))
-    def extract(rawLst: List[(Int,Int)], extracted: List[(Int,Int)]): List[(Int,Int)] =
+    def gcf(lst: List[List[(Int,Int)]], filtered: List[(Int,Int)]): List[(Int,Int)] =
+      if (lst.isEmpty) filtered
+      else gcf(lst.tail, intersection(lst.head, filtered))
+    def intersection(rawLst: List[(Int,Int)], filtered: List[(Int,Int)]): List[(Int,Int)] =
       for {
-        ex <- extracted
-        rl <- rawLst
-        if ex._1 == rl._1
-      } yield (ex._1, if (ex._2 <= rl._2) ex._2 else rl._2)
+        f <- filtered
+        r <- rawLst
+        if f._1 == r._1
+      } yield (f._1, if (f._2 <= r._2) f._2 else r._2)
 
     def tupleToList(gcf: List[(Int,Int)]): List[String] =
       if (gcf.isEmpty) Nil
