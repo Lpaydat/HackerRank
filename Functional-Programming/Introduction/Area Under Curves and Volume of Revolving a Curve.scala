97663dd1d4fbcb51b1692819830e084c29c8be95
iMadooc <i.am.madooc@gmail.com>
2016-03-11 16:11:44 +0700

    Update Area Under Curves and Volume of Revolving a Curve.scala

--- Functional-Programming/Introduction/Area Under Curves and Volume of Revolving a Curve.scala
+++ Functional-Programming/Introduction/Area Under Curves and Volume of Revolving a Curve.scala
@@ -9,12 +9,12 @@
     0.001 * Math.PI * Math.pow(fx(coefficients, powers, x), 2)
 
   def summation(
-       func: (List[Int], List[Int], Double) => Double,
-       upperLimit: Int,
-       lowerLimit: Int,
-       coefficients: List[Int],
-       powers: List[Int]
-     ): Double = {
+    func: (List[Int], List[Int], Double) => Double,
+    upperLimit: Int,
+    lowerLimit: Int,
+    coefficients: List[Int],
+    powers: List[Int]
+  ): Double = {
     def compute(a: Double, b: Double): Double =
       if (a > b) 0
       else func(coefficients, powers, a) + compute(a + 0.001, b)
