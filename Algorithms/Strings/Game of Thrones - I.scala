ec3b2667eb32141163cb8cd372d9692b9b1691e3
iMadooc <i.am.madooc@gmail.com>
2016-03-16 19:52:57 +0700

    Update Game of Thrones - I.scala

--- Algorithms/Strings/Game of Thrones - I.scala
+++ Algorithms/Strings/Game of Thrones - I.scala
@@ -1,12 +1,12 @@
 object Solution {
   
-  def isAnagram(s: String) = {
+  def canLock(s: String) = {
     val countedChar = s.toList.groupBy(identity).mapValues(_.size).toList
     val oddChar = countedChar.count(_._2 % 2 != 0)
     if (oddChar > 1) "NO" else "YES"
   }
 
   def main(args: Array[String]) {
-    println(isAnagram(readLine()))
+    println(canLock(readLine()))
   }
 }
