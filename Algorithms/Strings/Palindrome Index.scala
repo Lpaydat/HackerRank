cabde62a824b99d681b26c0ed6bb081da023b2fd
iMadooc <i.am.madooc@gmail.com>
2016-03-17 22:14:44 +0700

    Update Palindrome Index.scala

--- Algorithms/Strings/Palindrome Index.scala
+++ Algorithms/Strings/Palindrome Index.scala
@@ -2,10 +2,7 @@ object Solution {
   
   def palindromeIndex(str: String): Int = {
     def removeIndex(s: String, p: Int): String = s.take(p) + s.drop(p + 1)
-    def isPalindrome(s: String): Int = {
-      val combined = (s zip s.reverse).toList
-      combined.indexWhere(c => c._1 != c._2)
-    }
+    def isPalindrome(s: String): Int = (s zip s.reverse).indexWhere(c => c._1 != c._2)
     val f = isPalindrome(str)
     val s = str.length - f - 1
     (f, s) match {
