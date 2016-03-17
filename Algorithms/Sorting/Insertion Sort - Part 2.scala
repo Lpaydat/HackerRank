b22ac412b433a0b64005f5e5c27fec310f3dfdc8
iMadooc <i.am.madooc@gmail.com>
2016-03-18 00:27:49 +0700

    Create Insertion Sort - Part 2.scala

---
+++ Algorithms/Sorting/Insertion Sort - Part 2.scala
@@ -0,0 +1,17 @@
+object Solution {
+  
+  def insertionSort(arr: Array[Int]): String = {
+    val lastIndex = arr.length
+    def recur(ar: Array[Int], n: Int): List[String] = {
+      if (n > lastIndex) Nil
+      else (ar.take(n).sorted ++ ar.drop(n)).mkString(" ") :: recur(ar, n + 1)
+    }
+    recur(arr, 2).mkString("\n")
+  }
+
+  def main(args: Array[String]) {
+    val n = readInt()
+    val input = readLine().split(" ").map(_.toInt)
+    println(insertionSort(input))
+  }
+}
